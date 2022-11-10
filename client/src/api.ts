2ab282f9320dc8ba82784f5b9b47f41381100e87
mamonde456 <mamonde456@gmail.com>
2022-11-10 19:50:44 +0900

    client_id >> env

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,6 +1,6 @@
 export const backgroundImgFn = async (query: string) => {
   const config = {
-    client_id: "YDIkdn5nE19BVaUr1L9Cnh12iVuX0BBdA3TwzBCZT8w",
+    client_id: process.env.REACT_APP_BG_CLIENT_ID,
     query,
     count: "10",
   };
@@ -15,5 +15,11 @@ export const backgroundImgFn = async (query: string) => {
 
 export const quotesFn = async () => {
   const response = await fetch("https://api.adviceslip.com/advice");
+  return await response.json();
+};
+
+export const weatherApi = async (lat: number, lon: number) => {
+  const endUrl = `${process.env.REACT_APP_WEATHER_API_URL}?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`;
+  const response = await fetch(endUrl);
   return await response.json();
 };
